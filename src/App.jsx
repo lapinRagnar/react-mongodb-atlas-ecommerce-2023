1a202f474556293687ac2d262572baf5baccfea8
lapinRagnar <lapinragnar@gmail.com>
2023-11-02 21:41:54 +0100

    2 - ajouter des pages et la des routes - page structure et ajouter les liens de navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import LoginSignup from './pages/LoginSignup'
 
 function App() {
 
 
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Shop />}  />
+          <Route path="/men" element={<ShopCategory category="men" />}  />
+          <Route path="/women" element={<ShopCategory category="women" />}  />
+          <Route path="/kids" element={<ShopCategory category="kids" />}  />
+
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />}/>
+          </Route>
+          <Route path="/cart" element={<Cart />}  />
+          <Route path="/login" element={<LoginSignup />}  />
+        </Routes>
+      
+      </BrowserRouter>
   
     </div>
   )
