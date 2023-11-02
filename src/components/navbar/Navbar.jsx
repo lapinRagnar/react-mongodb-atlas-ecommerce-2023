8c59b9e80e2f2bbaa59c23cca5fa67fd3f7068f1
lapinRagnar <lapinragnar@gmail.com>
2023-11-02 20:44:24 +0100

    1- la navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import './Navbar.css'
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
+import { useState } from 'react'
 const Navbar = () => {
+  const [menu,setMenu] = useState("shop")
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt='logo'/>
+        <p>lapinRagnar</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>Shop {menu==="shop" ? <hr/> : ""} </li>
+        <li onClick={() => setMenu("men")}>Men {menu==="men" ? <hr/> : ""}</li>
+        <li onClick={() => setMenu("women")}>Women {menu==="women" ? <hr/> : ""}</li>
+        <li onClick={() => setMenu("kids")}>Kids {menu==="kids" ? <hr/> : ""}</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart" />
+        <div className='nav-cart-count'>0</div>
+        </div>
+
+
+    </div>
   )
 }
 
