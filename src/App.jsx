155c35066a31fd1b4dddf01106ce537de3cb58dd
lapinRagnar <lapinragnar@gmail.com>
2023-11-06 16:18:52 +0100

    Shop Context Provider & trié par women men kids

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,44 @@ import Product from './pages/Product'
 import Cart from './pages/Cart'
 import LoginSignup from './pages/LoginSignup'
 import Footer from './components/footer/Footer'
+import men_banner from './components/assets/banner_mens.png'
+import women_banner from './components/assets/banner_women.png'
+import kids_banner from './components/assets/banner_kids.png'
+
+import { ShopContext } from './context/ShopContext'
+import all_product from './components/assets/all_product'
 
 function App() {
 
 
+
   return (
     <div>
-      <BrowserRouter>
+
+      <ShopContext.Provider value={{men_banner, women_banner, kids_banner, all_product}}>
+
+        <BrowserRouter>
+        
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Shop />}  />
+            <Route path="/men" element={<ShopCategory banner={men_banner} all_product={all_product}  category="men" />}  />
+            <Route path="/women" element={<ShopCategory banner={women_banner} all_product={all_product}  category="women" />}  />
+            <Route path="/kids" element={<ShopCategory banner={kids_banner} all_product={all_product}  category="kid" />}  />
+
+            <Route path="/product" element={<Product />}>
+              <Route path=":productId" element={<Product />}/>
+            </Route>
+            <Route path="/cart" element={<Cart />}  />
+            <Route path="/login" element={<LoginSignup />}  />
+          </Routes>
+
+          <Footer />
+        
+        </BrowserRouter>
       
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Shop />}  />
-          <Route path="/men" element={<ShopCategory category="men" />}  />
-          <Route path="/women" element={<ShopCategory category="women" />}  />
-          <Route path="/kids" element={<ShopCategory category="kids" />}  />
-
-          <Route path="/product" element={<Product />}>
-            <Route path=":productId" element={<Product />}/>
-          </Route>
-          <Route path="/cart" element={<Cart />}  />
-          <Route path="/login" element={<LoginSignup />}  />
-        </Routes>
-
-        <Footer />
-      
-      </BrowserRouter>
+        </ShopContext.Provider>
   
     </div>
   )
--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,7 +1,35 @@
 
-const ShopCategory = () => {
+import './css/ShopCategory.css'
+import dropdown_icon from '../components/assets/dropdown_icon.png'
+import all_product from '../components/assets/all_product'
+import Item from '../components/item/Item'
+
+const ShopCategory = (props) => {
+
   return (
-    <div>ShopCategory</div>
+    <div className="shop-category">
+      <img className='shopcategory-banner' src={props.banner } alt="banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>afficher [1-12]</span> sur 36 produits
+        </p>
+        <div className="shopcategory-sort">
+          trié par <img src={dropdown_icon} alt="dropdown" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return <Item key={i} {...item} />
+          } else {
+            return null
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Plus ...
+      </div>
+    </div>
   )
 }
 
