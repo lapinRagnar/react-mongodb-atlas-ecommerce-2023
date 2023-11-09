a7f80adb3bf4d7c10fbb4053eedec2d83506c04d
lapinRagnar <lapinragnar@gmail.com>
2023-11-09 20:43:05 +0100

    reparer le style de la page shop

--- src/components/newCollections/NewCollections.jsx
+++ src/components/newCollections/NewCollections.jsx
@@ -4,7 +4,7 @@ import Item from "../item/Item"
 const NewCollections = () => {
   return (
     <div className="new-collections">
-      <h1>NOUVEAUX COLLECTIONS</h1>
+      <h1>NOUVELLES COLLECTIONS</h1>
       <div className="collections">
         { newCollections.map((item, i) => {
           return <Item key={i} {...item} />
